82985f235f0667a7be3582d24a6226cd09390ce1
Daniel <daniel.j.fletcher2@gmail.com>
2017-07-10 18:00:08 -0700

    Add comments to num_paths problem

--- num_paths.py
+++ num_paths.py
@@ -1,32 +1,32 @@
 def count_paths(grid):
     num_rows = len(grid)
     num_cols = len(grid[0])
-
-    row = num_rows - 1
-    col = num_cols - 1
-
+    # create new grid of same dimensions to memoize number of paths from each square
     memo = [[0 for j in range(num_cols)] for i in range(num_rows)]
 
+    # start at bottom right and work our way back to origin row by row
+    row = num_rows - 1
     while row >= 0:
         col = num_cols - 1
         while col >= 0:
             if grid[row][col]:
+                # if current square is a 1 then memoize num paths from this square
                 if row == num_rows - 1 and col == num_rows - 1:
-                    # if bottom right, memoize 1 path
+                    # initialize bottom  1 path
                     memo[row][col] = 1
                 elif row == num_rows - 1:
-                    # if bottom row, only look right
+                    # if bottom row, the only path is to the right
                     memo[row][col] = memo[row][col + 1]
                 elif col == num_cols - 1:
-                    # if rightmost column, only look down
+                    # if rightmost column, the only path is down
                     memo[row][col] = memo[row + 1][col]
                 else:
-                    # for all other squares sum the squares to left and right
+                    # sum the squares currently below and to the right
                     memo[row][col] = memo[row + 1][col] + memo[row][col + 1]
             else:
+                # if current square is a 0, then memoize that there are 0 paths from this square
                 memo[row][col] = 0
             col -= 1
-
         row -= 1
 
     return memo[0][0]
