e6b347b3b74c573065e8fee018e372a961d59e13
Daniel <daniel.j.fletcher2@gmail.com>
2017-07-18 16:26:27 -0700

    Write #checkBST function

--- hacker_rank.py
+++ hacker_rank.py
@@ -13,3 +13,19 @@ def ransom_note(magazine, ransom):
         else:
             mag_words[word] -= 1
     return True
+
+
+# Source: https://www.hackerrank.com/challenges/ctci-is-binary-search-tree?h_r=next-challenge&h_v=zen
+# Node is defined as
+# class node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.left = None
+#         self.right = None
+# Given a root node, check whether it is a valid Binary Search Tree
+def checkBST(root, mini=None, maxi=None):
+    if not root or not root.left and not root.right:
+        return True
+    if mini and mini >= root.data or maxi and maxi <= root.data:
+        return False
+    return checkBST(root.left, mini, root.data) and checkBST(root.right, root.data, maxi)
