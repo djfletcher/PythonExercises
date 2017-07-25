fc640b677e0f90987a523ac5cfc1655be2d21e4d
Daniel <daniel.j.fletcher2@gmail.com>
2017-07-24 18:00:01 -0700

    Solve most cases of minimum edits

--- dynamic_pragramming.py
+++ dynamic_pragramming.py
@@ -4,17 +4,22 @@ def minimum_edit_distance(str1, str2):
     memo = [[]]
     for str1_idx in range(len(str1)):
         for str2_idx in range(len(str2)):
-            if str1_idx == 0 or str2_idx == 0:
-                memo[str1_idx][str2_idx] = 0 if str1[str1_idx] == str2[str2_idx] else 1
+            if str1_idx == 0:
+                memo[str1_idx].append(0 if str1[str1_idx] == str2[str2_idx] else 1)
+            elif str2_idx == 0:
+                memo.append([0] if str1[str1_idx] == str2[str2_idx] else [1])
             else:
                 if str1[str1_idx] == str2[str2_idx]:
-                    memo[str1_idx][str2_idx] = memo[str1_idx - 1][str2_idx - 1]
+                    memo[str1_idx].append(memo[str1_idx - 1][str2_idx - 1])
                 else:
-                    memo[str1_idx][str2_idx] = 1 + min(
+                    memo[str1_idx].append(1 + min(
                     memo[str1_idx][str2_idx - 1],
                     memo[str1_idx - 1][str2_idx],
                     memo[str1_idx - 1][str2_idx - 1]
-                    )
+                    ))
     return memo[len(str1) - 1][len(str2) - 1]
 
-print(minimum_edit_distance('abc', 'abb'))
+print(minimum_edit_distance('abc', 'abb') == 1)
+print(minimum_edit_distance('mob', 'moby') == 1)
+print(minimum_edit_distance('abc', 'def') == 3)
+print(minimum_edit_distance('hope', 'open') == 2)
