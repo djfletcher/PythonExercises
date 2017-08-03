154027bc3dd3a25733b0adac3e84e3a76023ee08
Daniel <daniel.j.fletcher2@gmail.com>
2017-08-02 22:39:44 -0700

    complete problem

--- dynamic_pragramming.py
+++ dynamic_pragramming.py
@@ -19,7 +19,66 @@ def minimum_edit_distance(str1, str2):
                     ))
     return memo[len(str1) - 1][len(str2) - 1]
 
-print(minimum_edit_distance('abc', 'abb') == 1)
-print(minimum_edit_distance('mob', 'moby') == 1)
-print(minimum_edit_distance('abc', 'def') == 3)
-print(minimum_edit_distance('hope', 'open') == 2)
+# print(minimum_edit_distance('abc', 'abb') == 1)
+# print(minimum_edit_distance('mob', 'moby') == 1)
+# print(minimum_edit_distance('abc', 'def') == 3)
+# print(minimum_edit_distance('hope', 'open') == 2)
+
+
+# Find largest square submatrix
+def max_submatrix(matrix):
+    memo = [[0] * len(matrix[0]) for i in range(len(matrix))]
+    for row in range(len(matrix)):
+        for col in range(len(matrix[0])):
+            if row == 0 or col == 0:
+                memo[row][col] = 0
+            elif matrix[row][col] == 0:
+                memo[row][col] = 0
+            else:
+                memo[row][col] = 1 + min(
+                    memo[row][col - 1],
+                    memo[row - 1][col - 1],
+                    memo[row - 1][col]
+                )
+    max = 0
+    for row in memo:
+        for val in row:
+            if val > max:
+                max = val
+    return max
+
+test1 = [
+  [1, 0, 1, 0, 1, 0],
+  [1, 0, 1, 1, 1, 1],
+  [0, 1, 1, 1, 1, 1],
+  [0, 0, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1]
+]
+
+test2 = [
+  [1, 0, 1, 0, 1, 0],
+  [1, 0, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1],
+  [0, 0, 1, 1, 1, 1],
+  [1, 1, 1, 1, 0, 1]
+]
+
+test3 = [
+  [1, 0, 1, 0, 1, 0],
+  [1, 0, 1, 1, 1, 0],
+  [0, 1, 1, 1, 1, 1]
+]
+
+test4 = [[]]
+
+test5 = [
+  [0, 1, 1],
+  [0, 0, 1],
+  [0, 1, 1]
+]
+
+print(max_submatrix(test1) == 4)
+print(max_submatrix(test2) == 3)
+print(max_submatrix(test3) == 2)
+print(max_submatrix(test4) == 0)
+print(max_submatrix(test5) == 1)
